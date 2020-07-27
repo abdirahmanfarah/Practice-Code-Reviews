504964f794ab52eb23d01643a5b1f42922266b1e
Abdi Farah <abdirahman.z.farah@gmail.com>
2020-07-27 10:36:13 -0700

    Permutation recursive

--- Python/cake2.py
+++ Python/cake2.py
@@ -1,36 +1,65 @@
-def CodelandUsernameValidation(str):
+# def CodelandUsernameValidation(str):
 
-    # code goes here
-    this_is_true = "true"
-    this_is_false = "false"
+#     # code goes here
+#     this_is_true = "true"
+#     this_is_false = "false"
 
-    dict_b = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-              'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
+#     dict_b = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+#               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'}
 
-    dict_a = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
-              'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "_"}
-    length_string = len(str)
-    if length_string > 25 and length_string < 5:
-        return this_is_false
-    print(str[-1])
-    for char in range(len(str)):
-        # print(str[0])
-        print(str[char])
-        # print(str[-1])
-        first_letter = str[0]
-        if first_letter not in dict_b:
-            print(str[0])
-            return this_is_false
+#     dict_a = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
+#               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "_"}
+#     length_string = len(str)
+#     if length_string > 25 and length_string < 5:
+#         return this_is_false
+#     print(str[-1])
+#     for char in range(len(str)):
+#         # print(str[0])
+#         print(str[char])
+#         # print(str[-1])
+#         first_letter = str[0]
+#         if first_letter not in dict_b:
+#             print(str[0])
+#             return this_is_false
 
-        elif str[-1] == '_':
-            return this_is_false
-        elif str[char] not in dict_a:
-            return this_is_false
+#         elif str[-1] == '_':
+#             return this_is_false
+#         elif str[char] not in dict_a:
+#             return this_is_false
 
-    return this_is_true
+#     return this_is_true
 
-    # return str
+#     # return str
 
 
-# keep this function call here
-print(CodelandUsernameValidation("u__hello_world123"))
+# # keep this function call here
+# print(CodelandUsernameValidation("u__hello_world123"))
+
+def get_permutations(string):
+    # Base case
+    if len(string) <= 1:
+        return set([string])
+
+    all_chars_except_last = string[:-1]
+    last_char = string[-1]
+
+    # Recursive call: get all possible permutations for all chars except last
+    permutations_of_all_chars_except_last = get_permutations(
+        all_chars_except_last)
+
+    # Put the last char in all possible positions for each of
+    # the above permutations
+    permutations = set()
+    for permutation_of_all_chars_except_last in permutations_of_all_chars_except_last:
+        for position in range(len(all_chars_except_last) + 1):
+            permutation = (
+                permutation_of_all_chars_except_last[:position]
+                + last_char
+                + permutation_of_all_chars_except_last[position:]
+            )
+            permutations.add(permutation)
+
+    return permutations
+
+
+print(get_permutations('abc'))
