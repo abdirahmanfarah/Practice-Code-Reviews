57973a2e450c8bb9a0286536111bd326d3945c16
Abdi Farah <abdirahman.z.farah@gmail.com>
2020-07-26 20:31:19 -0700

    cake2

--- Python/cake2.py
+++ Python/cake2.py
@@ -10,7 +10,7 @@ def CodelandUsernameValidation(str):
     dict_a = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y',
               'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "_"}
     length_string = len(str)
-    if length_string > 25 and length_string < 4:
+    if length_string > 25 and length_string < 5:
         return this_is_false
     print(str[-1])
     for char in range(len(str)):
